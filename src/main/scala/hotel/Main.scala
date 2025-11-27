0ec4f769eaa14c1b45a74c32e9dbaf74e2f32cae
favtan <48796686+favtan@users.noreply.github.com>
2025-11-28 01:06:30 +0800

    question 1 done

--- src/main/scala/hotel/Main.scala
+++ src/main/scala/hotel/Main.scala
@@ -9,15 +9,21 @@ object Main extends App:
 
   private val DataFile = "data/Hotel_Dataset.csv"
 
+  val bookings = loadBookings(DataFile)
+
   println("=" * 40)
   println("=== Hotel Booking Analysis ===")
   println("=" * 40)
 
-  val bookings = loadBookings(DataFile)
-
   println(s"Loaded ${bookings.size} bookings.")
   println("First 3 bookings test:")
   bookings.take(3).foreach(println)
+
+  val (topCountry, topCount) = mostBookedCountry(bookings)
+  println()
+  println("Q1: Country with the highest number of bookings:")
+  println(s"  $topCountry with $topCount bookings")
+
 
 private def loadBookings(path: String): Seq[HotelBooking] =
   implicit val codec: Codec = Codec("windows-1252")
@@ -36,3 +42,7 @@ private def loadBookings(path: String): Seq[HotelBooking] =
   finally
     source.close()
 
+private def mostBookedCountry(bookings: Seq[HotelBooking]): (String, Int) =
+    val grouped = bookings.groupBy(_.originCountry)
+    val counts  = grouped.view.mapValues(_.size).toMap
+    counts.maxBy { case (_, count) => count }
