fe8ae9efc018aea33f6633e798adad5dd366f97d
favtan <48796686+favtan@users.noreply.github.com>
2025-11-27 23:46:29 +0800

    HotelBooking case class

--- src/main/scala/hotel/HotelBooking.scala
+++ src/main/scala/hotel/HotelBooking.scala
@@ -1,1 +1,37 @@
+package hotel
+
+case class HotelBooking(
+  bookingId: String,
+  originCountry: String,
+  hotelName: String
+)
+
+object HotelBooking:
+
+  def headerIndex(headerLine: String, separator: Char = ','): Map[String, Int] =
+    headerLine
+      .split(separator)
+      .map(_.trim.toLowerCase)
+      .zipWithIndex
+      .toMap
+
+  def fromCsvLine(
+      line: String,
+      index: Map[String, Int],
+      separator: Char = ','
+  ): Option[HotelBooking] =
+    val cols = line.split(separator, -1).map(_.trim)
+
+    def col(nameLower: String): String =
+      cols(index(nameLower))      
+
+    try
+      val id      = col("booking id")
+      val origin  = col("origin country")
+      val hotel   = col("hotel name")
+      Some(HotelBooking(id, origin, hotel))
+    catch
+      case _: Throwable =>
+       
+        None
 
