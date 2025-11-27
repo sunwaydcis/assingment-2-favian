831386c9f6a61dfe5f472c50a4f925fe6aeca8c0
favtan <48796686+favtan@users.noreply.github.com>
2025-11-27 22:39:08 +0800

    count lines in CSV

--- src/main/scala/hotel/Main.scala
+++ src/main/scala/hotel/Main.scala
@@ -1,1 +1,14 @@
+package hotel
 
+import scala.io.Source
+
+object Main extends App:
+
+  private val DataFile = "Hotel_Dataset.csv"
+  println("=== Hotel Booking Analysis (Step 1) ===")
+
+  val source = Source.fromFile(DataFile)
+  try
+    val lineCount = source.getLines().size
+    println(s"Number of lines in CSV (including header): $lineCount")
+    source.close()
