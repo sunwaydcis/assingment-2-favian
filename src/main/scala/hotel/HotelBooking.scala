957bd47fd48e14df45e877de7db53de09dba2b13
favtan <48796686+favtan@users.noreply.github.com>
2025-11-29 06:32:35 +0800

    add HotelBooking.scala fields

--- src/main/scala/hotel/HotelBooking.scala
+++ src/main/scala/hotel/HotelBooking.scala
@@ -1,9 +1,15 @@
 package hotel
 
 case class HotelBooking(
-  bookingId: String,
-  originCountry: String,
-  hotelName: String
+    bookingId: String,
+    originCountry: String,
+    destinationCountry: String,
+    hotelName: String,
+    bookingPrice: Double,
+    discountRate: Double,     
+    gst: Double,              
+    profitMargin: Double,    
+    numPeople: Int
 )
 
 object HotelBooking:
@@ -23,15 +29,39 @@ object HotelBooking:
     val cols = line.split(separator).map(_.trim)
 
     def col(nameLower: String): String =
-      cols(index(nameLower))      
+      cols(index(nameLower))
 
     try
       val id      = col("booking id")
       val origin  = col("origin country")
+      val dest    = col("destination country")
       val hotel   = col("hotel name")
-      Some(HotelBooking(id, origin, hotel))
+      val price   = col("booking price[sgd]").toDouble
+
+      val discountStr = col("discount")
+      val discountRate =
+        if discountStr.endsWith("%") then
+          discountStr.dropRight(1).toDouble / 100.0   
+        else
+          discountStr.toDouble
+
+      val gst     = col("gst").toDouble             
+      val margin  = col("profit margin").toDouble    
+      val people  = col("no. of people").toInt
+
+      Some(
+        HotelBooking(
+          id,
+          origin,
+          dest,
+          hotel,
+          price,
+          discountRate,
+          gst,
+          margin,
+          people
+        )
+      )
     catch
       case _: Throwable =>
-       
         None
-
