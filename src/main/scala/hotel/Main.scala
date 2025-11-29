b63596ff7d3a75ae78451808981dc2f00e8291a8
favtan <48796686+favtan@users.noreply.github.com>
2025-11-29 09:44:34 +0800

    reduced duplication and q3
    added reusables

--- src/main/scala/hotel/Main.scala
+++ src/main/scala/hotel/Main.scala
@@ -1,9 +1,7 @@
 package hotel
 
-import scala.io.Source
-import scala.io.Codec
+import scala.io.{Source, Codec}
 import java.nio.charset.CodingErrorAction
-
 
 object Main extends App:
 
@@ -19,60 +17,93 @@ object Main extends App:
   println("First 3 bookings test:")
   bookings.take(3).foreach(println)
 
+  // --- Q1 ---
   val (topCountry, topCount) = mostBookedCountry(bookings)
   println()
   println("Q1: Country with the highest number of bookings:")
   println(s"  $topCountry with $topCount bookings")
 
+  
   val (cheapestHotel, cheapestAvgPrice) = hotelWithLowestAveragePrice(bookings)
   println()
   println("Q2(a): Most economical hotel by booking price (lowest average price):")
   println(f"  $cheapestHotel with average price SGD $cheapestAvgPrice%.2f")
 
+  
   val (bestDiscountHotel, bestAvgDiscount) =
     hotelWithHighestAverageDiscount(bookings)
   println()
   println("Q2(b): Most economical hotel by discount (highest average discount):")
   println(f"  $bestDiscountHotel with average discount ${bestAvgDiscount * 100}%.2f%%")
 
+  
   val (lowestMarginHotel, lowestAvgMargin) =
     hotelWithLowestAverageProfitMargin(bookings)
   println()
   println("Q2(c): Most economical hotel by profit margin (lowest average margin):")
   println(f"  $lowestMarginHotel with average profit margin ${lowestAvgMargin * 100}%.2f%%")
 
+  
+  val (profitHotel, totalProfit) = mostProfitableHotel(bookings)
+  println()
+  println("Q3: Most profitable hotel (visitors * profit margin):")
+  println(f"  $profitHotel with estimated total profit SGD $totalProfit%.2f")
+
+
+private def averageByHotel(
+    bookings: Seq[HotelBooking]
+)(metric: HotelBooking => Double): Map[String, Double] =
+  bookings
+    .groupBy(_.hotelName)
+    .view
+    .mapValues { bs =>
+      val total = bs.iterator.map(metric).sum
+      total / bs.size
+    }
+    .toMap
+
+
 private def hotelWithLowestAveragePrice(
-      bookings: Seq[HotelBooking]
-  ): (String, Double) =
-    val byHotel = bookings.groupBy(_.hotelName)
-    val avgPriceByHotel =
-      byHotel.view.mapValues { bs =>
-        val total = bs.map(_.bookingPrice).sum
-        total / bs.size
-      }.toMap
-    avgPriceByHotel.minBy { case (_, avgPrice) => avgPrice }
+    bookings: Seq[HotelBooking]
+): (String, Double) =
+  val averages = averageByHotel(bookings)(_.bookingPrice)
+  averages.minBy(_._2)
+
 
 private def hotelWithHighestAverageDiscount(
-      bookings: Seq[HotelBooking]
-  ): (String, Double) =
-    val byHotel = bookings.groupBy(_.hotelName)
-    val avgDiscByHotel =
-      byHotel.view.mapValues { bs =>
-        val total = bs.map(_.discountRate).sum
-        total / bs.size
-      }.toMap
-    avgDiscByHotel.maxBy { case (_, avgDisc) => avgDisc }
+    bookings: Seq[HotelBooking]
+): (String, Double) =
+  val averages = averageByHotel(bookings)(_.discountRate)
+  averages.maxBy(_._2)
+
 
 private def hotelWithLowestAverageProfitMargin(
-      bookings: Seq[HotelBooking]
-  ): (String, Double) =
-    val byHotel = bookings.groupBy(_.hotelName)
-    val avgMarginByHotel =
-      byHotel.view.mapValues { bs =>
-        val total = bs.map(_.profitMargin).sum
-        total / bs.size
-      }.toMap
-    avgMarginByHotel.minBy { case (_, avgMargin) => avgMargin }
+    bookings: Seq[HotelBooking]
+): (String, Double) =
+  val averages = averageByHotel(bookings)(_.profitMargin)
+  averages.minBy(_._2)
+
+
+private def totalProfitByHotel(
+    bookings: Seq[HotelBooking]
+): Map[String, Double] =
+  bookings
+    .groupBy(_.hotelName)
+    .view
+    .mapValues { bs =>
+      bs.iterator.map { b =>
+        b.bookingPrice * b.profitMargin * b.numPeople
+      }.sum
+    }
+    .toMap
+
+
+private def mostProfitableHotel(
+    bookings: Seq[HotelBooking]
+): (String, Double) =
+  val profitMap = totalProfitByHotel(bookings)
+  profitMap.maxBy(_._2)
+
 
 private def loadBookings(path: String): Seq[HotelBooking] =
   implicit val codec: Codec = Codec("windows-1252")
@@ -91,7 +122,8 @@ private def loadBookings(path: String): Seq[HotelBooking] =
   finally
     source.close()
 
+
 private def mostBookedCountry(bookings: Seq[HotelBooking]): (String, Int) =
-    val grouped = bookings.groupBy(_.originCountry)
-    val counts  = grouped.view.mapValues(_.size).toMap
-    counts.maxBy { case (_, count) => count }
+  val grouped = bookings.groupBy(_.originCountry)
+  val counts  = grouped.view.mapValues(_.size).toMap
+  counts.maxBy { case (_, count) => count }
