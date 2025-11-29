fc58454c9486b22315da1684ea4414c1eadece2c
favtan <48796686+favtan@users.noreply.github.com>
2025-11-29 09:54:23 +0800

    clean up code

--- src/main/scala/hotel/HotelAnalysis.scala
+++ src/main/scala/hotel/HotelAnalysis.scala
@@ -1,1 +1,56 @@
+package hotel
+
+object HotelAnalysis:
+
+  private def averageByHotel(
+      bookings: Seq[HotelBooking]
+  )(
+      metric: HotelBooking => Double
+  ): Map[String, Double] =
+    bookings
+      .groupBy(_.hotelName)
+      .view
+      .mapValues { bs =>
+        val total = bs.iterator.map(metric).sum
+        total / bs.size
+      }
+      .toMap
+
+  def hotelWithLowestAveragePrice(
+      bookings: Seq[HotelBooking]
+  ): (String, Double) =
+    val averages = averageByHotel(bookings)(_.bookingPrice)
+    averages.minBy(_._2)
+
+  def hotelWithHighestAverageDiscount(
+      bookings: Seq[HotelBooking]
+  ): (String, Double) =
+    val averages = averageByHotel(bookings)(_.discountRate)
+    averages.maxBy(_._2)
+
+  def hotelWithLowestAverageProfitMargin(
+      bookings: Seq[HotelBooking]
+  ): (String, Double) =
+    val averages = averageByHotel(bookings)(_.profitMargin)
+    averages.minBy(_._2)
+
+  private def totalProfitByHotel(
+      bookings: Seq[HotelBooking]
+  ): Map[String, Double] =
+    bookings
+      .groupBy(_.hotelName)
+      .view
+      .mapValues { bs =>
+        bs.iterator.map { b =>
+          b.bookingPrice * b.profitMargin * b.numPeople
+        }.sum
+      }
+      .toMap
+
+  def mostProfitableHotel(
+      bookings: Seq[HotelBooking]
+  ): (String, Double) =
+    val profitMap = totalProfitByHotel(bookings)
+    profitMap.maxBy(_._2)
+
 
